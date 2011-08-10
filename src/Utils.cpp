e9a0af7a85b304b5d69dca00215e9c138b471e45
Jarupat Jisarojito <guitarnana@gmail.com>
2011-08-10 21:39:19 +1000

    Added Utils.hpp and Utils.cpp

---
+++ src/Utils.cpp
@@ -0,0 +1,27 @@
+/*
+ * Utils.cpp
+ *
+ *  Created on: Aug 10, 2011
+ *      Author: nana
+ */
+
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+
+#include "Defs.hpp"
+
+int getHops(Coordinate a, Coordinate b) {
+   return fabs(a.x - b.x) + fabs(a.y - b.y);
+}
+
+double uniform_0_1() {
+   return (double) rand() / RAND_MAX;
+}
+
+int uniform_n(int n) {
+   assert(n > 0);
+   return (int) (uniform_0_1() * (n));
+   //return uniform_0_1() * (n-1) + 1; //from [1,...,n]
+}
---
+++ src/Utils.hpp
@@ -0,0 +1,10 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+int getHops(Coordinate a, Coordinate b);
+
+double uniform_0_1();
+
+int uniform_n(int n);
+
+#endif
